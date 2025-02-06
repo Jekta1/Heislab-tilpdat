0c792711f0b3d428bb9aeef833919ec479c4d66f
Atle <aabystro@stud.ntnu.no>
2025-02-06 10:39:56 +0100

    added plans for module designs

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -12,30 +12,84 @@ main driver: manages the different modules, dermining when they should do which 
 
     State finder: Run at program start to initiate a defined state. Initiates a state tracker in its place once a defined state has been reached
 
-    State tracker (input manager): interfaces w. harware to update the current state of the system. 
+    State tracker (input manager): interfaces w. hardware to update the current state of the system. 
         - current floor
         - obstruction
         - stop-button
         - ect.
 
-    Instruction queue:
+    Helper Module: Instruction queue:
         - Add to queue given inputs (entry point is the State tracker)
-        - Internal logic for queue: what instructions should be executed first (entry point is the main driver)
+        - Internal logic for queue: what instructions should be executed first (entry point is adding to queue)
         - Recomends next floor to visit (entry point is the Logic controller)
-        - Clears queue when requested (entry point is the Locig controller)
+        - Clears queue when requested (entry point is the Logic controller)
 
 
     Logic controller #split maybe?: 
         - initiate timed stop if a floor with a relevant instruction in queue has been reached
         - delays the timed stop while an obstruction is active
         - sets outputs to a halt when requested
-        - sends ouput instructions to the output manger
+        - creates ouput instructions to the output manger
         - sets all relevant lights
         - interfaces with instruction queue
 
     Output manager:
         - interfaces w. hardware to realise instructions
         - extra failsafe that outputs are not given if stop or obstruction are active
+
+
+
+Module designs:
+
+Main Driver:
+    - public state, queue, outputs init() - initialises the state, queue and outputs
+    - public void run() - entry point, runs either state finder or driver depending on if a state has been found
+        - private bool stateFinder(&state, &outputs) - runs pipeline to find a defined state: first running state finder. returns wether a state has been found
+        - private void driver(&state, &queue, &outputs) - runs pipeline to manage the system: first running state tracker, then logic controller, then output manager
+
+State finder:
+    - public bool checkState(&state, &outputs) - interfacing, public function. returns a bool indicating wether the state has been reached. calls
+        - private void setOutputs(&outputs) - interfaces with the ouput manager to move the elevator
+        - private void initialiseState(&state) - sets the initial state of the system, once a state has been found. 
+            Once accived, the main driver will override the state finder with the state tracker and logic controller
+
+State tracker:
+    - public struct representing the state of the system
+    - private struct representing buttons pressed
+    - public updateState(&state, &queue) - entry point, updates the state of the system by calling these sub-functions
+                also modifies a reference to the queue struct, adding new instructions to it
+        - private getFloorState(&state) - updates the current floor and wether the elevator is at a floor
+        - private getObstructionState(&state) - updates the obstruction state
+        - private getStopButtonState(&state) - updates the stop button state
+        - private getButtonPresses(&queue) - creates an internal struct representing the buttons pressed
+            - interfaces to Instruction queue, adding new instructions to it and performing internal logic for the queue
+
+Instruction queue:
+    - public struct representing the queue. contains a list of instructions
+    - public void addInstruction(&queue, const &instruction, const &state) - entry point, adds a new instruction to the queue. calls sortInstructions before returning
+        - private void sortInstructions(&queue) - sorts the instructions in the queue, deciding which order they should be performed in
+
+    public clearQueue(&queue) - entry point, clears the queue
+    public clearInstruction(&queue, const int currentFloor) - entry point, clears the instruction at the current floor
+
+Logic Controller:
+    - public void controll(&state, &queue, &outputs) - entry point for controll. 
+        determines which sub-functions to call, given the state. also clears any instructions that end at the current floor
+        
+        - private bool manageTimer(&state) - if the timer is not acitve desipite having reached the target, activates tge timer. 
+            checks for conditions that should reset the timer and returns a bool indicating wether the timer is active, eg. we should wait
+        - private void setTarget(const &state, const &queue, &outputs) - sets the target floor of the elevator.
+        - private void setLights(const &state, &outputs) - updates output light instructions
+        - private void setMotorState(const &state, &outputs) - sets the motor state of the elevator, based on weather the elevator should move or not and what the target is
+
+Output manager:
+    - public void realiseInstructions(const &outputs) - entry point. asserts no illigal outputs are given
+        - private void setMotor(const &outputs) - sets the motor direction and turns it on/off
+        - private void setLights(const &outputs) - sets all lights of the elevator:
+            - floor lights
+            - button lights
+            - open door light
+
 */
 
 
